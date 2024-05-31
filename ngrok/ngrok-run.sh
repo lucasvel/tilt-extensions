7bd26e7a099aa108c47bd14a14e44c29900185ac
Lucas Velasco <3720144+lucasvel@users.noreply.github.com>
2024-05-31 16:57:40 -0300

    Update ngrok-run.sh

--- ngrok/ngrok-run.sh
+++ ngrok/ngrok-run.sh
@@ -10,9 +10,9 @@ default_config_file="${TILT_NGROK_DEFAULT_CONFIG_FILE:-$HOME/.config/ngrok/ngrok
 
 if [[ -f "$default_config_file" ]]; then
     args+=("--config" "$default_config_file")
+else
+    args+=("--config" "$config_file")
 fi
-
-args+=("--config" "$config_file")
 
 if grep -q "addr" "$config_file"; then
     args+=("--all")
